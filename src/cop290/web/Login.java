c78100ae8e11d92e6f91b5bccb7d91d4bcd5eb9a
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2016-03-18 14:22:49 +0530

    JSON responses

--- src/cop290/web/Login.java
+++ src/cop290/web/Login.java
@@ -2,13 +2,14 @@ package cop290.web;
 
 import java.io.IOException;
 
+import javax.json.Json;
+import javax.json.JsonObject;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 /**
  * Servlet implementation class Login
  */
@@ -32,7 +33,15 @@ public class Login extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletOutputStream sop = response.getOutputStream();
+        JsonObject result= Json.createObjectBuilder()
+                .add("result","FAILED")
+                .add("user","")
+                .add("attempt",attempt++)
+                .build();
+
+        response.setContentType("application/json");
+        response.getOutputStream().print(result.toString());
+        /*
         sop.println("<!DOCTYPE html>");
         sop.println("<html>");
         sop.println("<head>");
@@ -42,6 +51,7 @@ public class Login extends HttpServlet {
         sop.println("<h1>Login page</h1><p>Attempt Number " + attempt++ + "</p>");
         sop.println("</body>");
         sop.println("</html>");
+        */
     }
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
