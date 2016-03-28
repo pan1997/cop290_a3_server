8b79820057d7a632ecca6863cb91b472e7881d12
Pankaj Kumar <pankajkumaryadav1997@gmail.com>
2016-03-28 18:57:38 +0530

    Addusers corrected

--- src/cop290/web/AddUserServlet.java
+++ src/cop290/web/AddUserServlet.java
@@ -23,29 +23,25 @@ public class AddUserServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session=request.getSession();
-        JsonObject user=(JsonObject) session.getAttribute("user");
-        if(user==null||user.getInt("group_id")!=1)
-            response.getOutputStream().print(Json.createObjectBuilder().add("SUCCESS",false).add("MESSAGE",user==null?"NO LOGIN":"NO PRIVELAGE").build().toString());
-        else{
-            String first_name=request.getParameter("first_name");
-            String last_name=request.getParameter("last_name");
-            String login=request.getParameter("login");
-            String password=request.getParameter("password");
-            String group_id=request.getParameter("group_id");
-            String department_id=request.getParameter("department_id");
-            String entry_number=request.getParameter("entry_number");
-            String hostel_id=request.getParameter("hostel_id");
-
-            try{
-                Connection conn=tmpclass.ds.getConnection();
-                Statement smt=conn.createStatement();
-                String cmd="INSERT INTO Users(group_id, first_name, last_name, login, password, department_id, hostel_id, entry_number) VALUES " +
-                        "("+group_id+",'"+first_name+"','"+last_name+"','"+login+"','"+password+"',"+department_id+","+hostel_id+",'"+entry_number+"')";
-                System.out.println(cmd);
+        HttpSession session = request.getSession();
+        JsonObject user = (JsonObject) session.getAttribute("user");
+        if (user == null || user.getInt("group_id") != 1)
+            response.getOutputStream().print(Json.createObjectBuilder().add("SUCCESS", false).add("MESSAGE", user == null ? "NO LOGIN" : "NO PRIVELAGE").build().toString());
+        else {
+            String first_name = request.getParameter("first_name");
+            String last_name = request.getParameter("last_name");
+            String login = request.getParameter("login");
+            String password = request.getParameter("password");
+            String group_id = request.getParameter("group_id");
+            String department_id = request.getParameter("department_id");
+            String entry_number = request.getParameter("entry_number");
+            String hostel_id = request.getParameter("hostel_id");
+            try {
+                Connection conn = tmpclass.ds.getConnection();
+                Statement smt = conn.createStatement();
                 smt.execute("INSERT INTO Users(group_id, first_name, last_name, login, password, department_id, hostel_id, entry_number) VALUES " +
-                        "("+group_id+",'"+first_name+"','"+last_name+"','"+login+"','"+password+"',"+department_id+","+hostel_id+",'"+entry_number+"')");
-            }catch (SQLException e){
+                        "(" + group_id + ",'" + first_name + "','" + last_name + "','" + login + "','" + password + "'," + department_id + "," + hostel_id + ",'" + entry_number + "')");
+            } catch (SQLException e) {
                 e.printStackTrace();
                 throw new ServletException(e);
             }
